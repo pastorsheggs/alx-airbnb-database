ae76e65922cba750ef3489d7bc5a51a6e4123bf6
pastorsheggs <bamidel692@gmail.com>
2025-07-03 22:01:24 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
--- Initial Query: Retrieves all bookings with user, property, and payment details
+-- Initial Query: Get all bookings with user, property, and payment details
 SELECT 
     bookings.id AS booking_id,
     bookings.start_date,
@@ -15,4 +15,26 @@ SELECT
 FROM bookings
 JOIN users ON bookings.user_id = users.id
 JOIN properties ON bookings.property_id = properties.id
-JOIN payments ON bookings.id = payments.booking_id;
+JOIN payments ON bookings.id = payments.booking_id
+WHERE payments.status = 'completed' AND bookings.start_date >= '2024-01-01';
+
+-- Analyze the query’s performance
+EXPLAIN 
+SELECT 
+    bookings.id AS booking_id,
+    bookings.start_date,
+    bookings.end_date,
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email,
+    properties.id AS property_id,
+    properties.title AS property_title,
+    properties.location,
+    payments.id AS payment_id,
+    payments.amount,
+    payments.status
+FROM bookings
+JOIN users ON bookings.user_id = users.id
+JOIN properties ON bookings.property_id = properties.id
+JOIN payments ON bookings.id = payments.booking_id
+WHERE payments.status = 'completed' AND bookings.start_date >= '2024-01-01';
